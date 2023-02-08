3f2388d3b99bf48714685cba69e2b09a285289ab
DivyaTirk03 <124503324+DivyaTirk03@users.noreply.github.com>
2023-02-08 17:52:12 +0530

    Update Printed.js

--- src/components/Categori/Printed.js
+++ src/components/Categori/Printed.js
@@ -8,7 +8,7 @@ function Printed() {
 
     let getProduct = async () => {
 
-        let result = await fetch(`https://helo-on-her-hair-api.onrender.com/v1/upload/search?name=Printed`, {
+        let result = await fetch(`https://helo-on-her-hair-api-q3v6.onrender.com/v1/upload/search?name=Printed`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
