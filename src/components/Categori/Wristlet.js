f6b3bd33f0868e45219282b0be1ddca40b32cdf4
DivyaTirk03 <124503324+DivyaTirk03@users.noreply.github.com>
2023-02-08 17:50:56 +0530

    Update Wristlet.js

--- src/components/Categori/Wristlet.js
+++ src/components/Categori/Wristlet.js
@@ -8,7 +8,7 @@ function Wristlet() {
 
     let getProduct = async () => {
 
-        let result = await fetch(`https://helo-on-her-hair-api.onrender.com/v1/upload/search?name=Wristlet`, {
+        let result = await fetch(`https://helo-on-her-hair-api-q3v6.onrender.com/v1/upload/search?name=Wristlet`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
