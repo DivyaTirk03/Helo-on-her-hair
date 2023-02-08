ea15b0eb1d94565fc625117fa2d599419cfaef41
DivyaTirk03 <124503324+DivyaTirk03@users.noreply.github.com>
2023-02-08 19:14:06 +0530

    Update Hair_Bows.js

--- src/components/Categori/Hair_Bows.js
+++ src/components/Categori/Hair_Bows.js
@@ -1,1 +1,58 @@
 
+import React, { useState, useEffect } from 'react'
+import KochoComp from '../KochoComp';
+import '../../css/Kocho.css'
+
+function Hair_Bows() {
+    const [product, setProduct] = useState([]);
+
+
+    let getProduct = async () => {
+
+        let result = await fetch(`https://helo-on-her-hair-api-q3v6.onrender.com/v1/upload/search?name=hairbows`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        );
+        result = await result.json();
+
+        if (result) {
+            console.log(result);
+            var data1 = result.data.reverse();
+            setProduct(data1);
+        }
+    }
+    useEffect(() => {
+        getProduct()
+    }, [])
+
+    return (
+        <div className='ds2'>
+            <div className="container pb-5">
+                <div className='row'>
+                    <h1 className='best'> Best Scunchies</h1>
+                    <div className='navigationbtn'>
+                        {/* <Filter /> */}
+                    </div>
+                    {
+                        product.map((val, ind) => {
+                            return <KochoComp
+                                productImage={val.productImage}
+                                productDetail={val.productDetail}
+                                productPrice={val.productPrice}
+                                productCategori={val.productCategori}
+                                id={val._id}
+                            />
+                        })
+                    }
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Hair_Bows;
